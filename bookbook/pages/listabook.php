d09c7c610ef79a0edad089d7484ab35c5c3c989b
Kastoli <kastoli@gmail.com>
2015-09-03 19:02:38 +1000

    List a Book
    Listing a book is currently working, although the linking table used to
    denote ownership of a book listing is not working due to no login system
    currently being implemented.

--- bookbook/pages/listabook.php
+++ bookbook/pages/listabook.php
@@ -4,11 +4,21 @@
 
 <h1>List A Book</h1>
 
+<?php
+	if(isset($_SESSION['error'])) {
+		echo '<p class="error">' . $_SESSION['error'] . '</p>';
+		unset($_SESSION['error']);
+	} elseif(isset($_SESSION['success'])) {
+		echo '<p class="success">' . $_SESSION['success'] . '</p>';
+		unset($_SESSION['success']);
+	}
+?>
+
 <form action='../pages/listabook-processing.php' method='post' id='listabook'>
 
-	<label>Book Title</label><input type='text'></input><div class='clearfix'></div>
-	<label>Author</label><input type='text'></input><div class='clearfix'></div>
-	<label>Edition</label><input type='text'></input><div class='clearfix'></div>
+	<label>Book Title</label><input type='text' name='title'></input><div class='clearfix'></div>
+	<label>Author</label><input type='text' name='author'></input><div class='clearfix'></div>
+	<label>Edition</label><input type='text' name='edition'></input><div class='clearfix'></div>
 	<input class='button' type='submit' name='submit' form='listabook'><div class='clearfix'></div>
 	
 </form>

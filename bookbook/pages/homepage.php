3aba3e4efce7e068f942be5281982b021026c0dd
Kastoli <kastoli@gmail.com>
2015-09-14 18:24:05 +1000

    Cleaning Up Old Code
    In preparation for some major additions to the website tomorrow I took
    the liberty of cleaning up some of the code. I've also removed the
    paragraph on the homepage and replaced it with a single line of dummy
    text, do not be alarmed, this is just to rectify some structure issues.

--- bookbook/pages/homepage.php
+++ bookbook/pages/homepage.php
@@ -1,11 +1,8 @@
 <?php
 	include '../includes/header.php';
 ?>
-
-<h1>Homepage</h1>
-</br>
-<h2>Welcome to BookBook, We are a service created by QUT students to be able to share textbooks with each other. Join us  by creating an account today!. Once a member of our site, you will have the ability to List and Search for books you need.</h2>
-
+			<h1>Homepage</h1>
+			<h2>Lorem</h2>
 <?php
 	include '../includes/footer.php';
 ?>
